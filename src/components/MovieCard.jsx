47d43906a7ff23400fe3dfe2ab338101a9e9ffd3
Andreas Copan <avcopan@gmail.com>
2023-07-21 18:22:31 -0400

    Restores onClick functionality to movie cards
    Eventually, I would like to add a modal dialog with movie info.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 export default function MovieCard({ movie }) {
+  const navigate = useNavigate();
+
   return (
-    <div className="card w-48 bg-base-100 shadow-xl mb-6">
+    <div
+      className="card w-48 bg-base-100 shadow-xl mb-6"
+      onClick={() => navigate(`/details/${movie.id}`)}
+    >
       <figure>
         <img src={movie.poster} alt={movie.title} />
       </figure>
